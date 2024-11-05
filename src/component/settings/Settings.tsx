772a3552cff504c78c9a61237a51421081528261
ivanjermakov <ivanjermakov1@gmail.com>
2024-11-05 21:42:36 +0100

    Nicer key setting process

--- src/component/settings/Settings.tsx
+++ src/component/settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { Accessor, Component, For, Match, Switch } from 'solid-js'
+import { Accessor, Component, For, Match, Switch, createSignal, onMount } from 'solid-js'
 import { Schema, StructuredValue, conformSchema, flattenValue } from '../../schema'
 import './Settings.module.scss'
 
@@ -56,16 +56,43 @@ export type SettingProps = {
 }
 
 const Setting: Component<SettingProps> = props => {
+    const [pressKeyMode, setPressKeyMode] = createSignal(false)
+    let keyInputRef: HTMLInputElement
+
     const onNumInput = (e: Event) => {
         const v = (e.target as HTMLInputElement).value
         const n = Number.parseInt(v)
         if (Number.isNaN(n)) return
         props.onChange?.({ ...props.setting, value: n } as any)
     }
+
+    const onKeyFocus = () => {
+        setPressKeyMode(true)
+        keyInputRef.value = '<press key>'
+    }
+    const onKeyFocusOut = () => {
+        if (pressKeyMode()) {
+            keyInputRef.value = (props.setting as any).value
+        }
+        setPressKeyMode(false)
+    }
     const onKeyInput = (e: Event) => {
         const value = (e.target as HTMLInputElement).value
         props.onChange?.({ ...props.setting, value } as any)
     }
+
+    onMount(() => {
+        keyInputRef?.addEventListener('keydown', e => {
+            e.preventDefault()
+            if (pressKeyMode()) {
+                const value = e.code
+                props.onChange?.({ ...props.setting, value } as any)
+                keyInputRef.value = value
+                setPressKeyMode(false)
+            }
+        })
+    })
+
     return (
         <tr title={props.setting.schema.description}>
             <td>{props.setting.schema.title}</td>
@@ -80,8 +107,13 @@ const Setting: Component<SettingProps> = props => {
                     </Match>
                     <Match when={props.setting.type === 'key'}>
                         <input
+                            ref={keyInputRef!}
                             type="text"
                             value={props.setting.type === 'key' && props.setting.value}
+                            onClick={onKeyFocus}
+                            onFocus={onKeyFocus}
+                            onFocusOut={onKeyFocusOut}
+                            onChange={e => e.preventDefault()}
                             onInput={onKeyInput}
                         />
                     </Match>
